81208522570a824a9983a400e6fd21a83033193e
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-25 16:16:00 +0000

    Users done Photo index done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,13 +1,14 @@
 class PhotosController < ApplicationController
 
   def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :username => :asc })
+
     render({ :template => "photo_templates/index.html.erb" })
   end
 
   def photo_details
-    @photo_id = params.fetch("an_id")
-    @details = Photo.where({ :id => @photo_id}).at(0)
-
     render({ :template => "photo_templates/photo_details.html.erb" })
   end
   
