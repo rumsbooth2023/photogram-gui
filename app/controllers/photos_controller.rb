097334be6fdd1254d8df6de23937745ff5f0d5b1
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-25 19:52:30 +0000

    CRud

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,12 +3,15 @@ class PhotosController < ApplicationController
   def index
     matching_photos = Photo.all
 
-    @list_of_photos = matching_photos.order({ :username => :asc })
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
 
     render({ :template => "photo_templates/index.html.erb" })
   end
 
   def photo_details
+    @url_photo_id = params.fetch("an_id")
+    @the_photo_id = Photo.where({ :id => @url_photo_id}).at(0)
+
     render({ :template => "photo_templates/photo_details.html.erb" })
   end
   
