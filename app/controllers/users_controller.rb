73d4a7e573fdb099a5996c407a5f8939a4bd8a72
rumsbooth baddam <rbaddam@chicagobooth.edu>
2023-04-25 05:15:18 +0000

    User Index Complete (except button) and user details kind of working - need to fix @photos in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
   def index
+    matching_users = User.all
+
+    @list_of_users = matching_users.order({ :username => :asc })
     render({ :template => "user_templates/index.html.erb" })
   end
 
   def user_details
     @user_name = params.fetch("an_id")
     @details = User.where({ :username => @user_name}).at(0)
+
+    @photos = Photo.where({ :owner_id => @details.id})
 
     render({ :template => "user_templates/user_details.html.erb" })
   end
